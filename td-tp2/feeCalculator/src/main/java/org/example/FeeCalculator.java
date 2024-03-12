b2f91321646e54f2bd5625aaf031133961befafe
eetchever004 <etcheverry.eliott10@gmail.com>
2024-03-12 18:31:02 +0100

    Algo liste de stratégies

--- td-tp2/feeCalculator/src/main/java/org/example/FeeCalculator.java
+++ td-tp2/feeCalculator/src/main/java/org/example/FeeCalculator.java
@@ -1,19 +1,40 @@
 package org.example;
 
+import java.util.List;
+
 public class FeeCalculator {
 
+    // créer une collection de stratégies
+    private static List<FeeStrategy> strategies = List.of (
+            new ChildFeeStrategy(),
+            new AdultFeeStrategy()
+        );
+
     public static double calculateFee(Visitor visitor, TicketType ticketType) {
+
         double fee = 0;
 
-        // calculate price for adults
-        if (visitor.age > 14) {
-            fee = new AdultFeeStrategy().calculate(ticketType);
+        // je donne la liste pour que cette recherche trouve la stratégie
+        // adaptée au visitor et calcule de prix correspondant
+        // algo : parcours complet de ma liste (malice) en comptant les stratégies qui conviennent
+        //          1 seule stratégie doit être trouvée, sinon on lèvera une exception
+        int i = 0; // indice de parcours
+        int compteurStrategiesOK = 0;
+        FeeStrategy strategy;
+        while (i < strategies.size()) {
+            strategy = strategies.get(i);
+            if (strategy.accept(visitor)) {
+                compteurStrategiesOK ++;
+                fee = strategy.calculate(ticketType);
+            }
+            i ++;
         }
 
-        // calculate price for children
-        if (visitor.age <= 14) {
-            fee = new ChildFeeStrategy().calculate(ticketType);
+        // retourner fee so 1 et 1 seule stratégie
+        if (compteurStrategiesOK != 1) {
+            throw new IllegalArgumentException();
         }
+
         return fee;
     }
 
